6225a45d0a19f6b3a2f6bfecadaec44d9c2ad296
Cedric Wolffs <cedric.wolffs@hhu.de>
2016-06-20 17:27:09 +0200

    Erstelle IntelliJ Projekt TDDTee
    Standard mit klassischem Hello World Aufruf in JavaFX

---
+++ src/TDDTeeMain.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TDDTeeMain extends Application
+{
+   public static void main(String[] args)
+   {
+      launch(args);
+   }
+
+   @Override
+   public void start(Stage primaryStage) throws Exception
+   {
+//      Sacht was
+      primaryStage.setTitle("Hello from the other side");
+      primaryStage.show();
+
+   }
+}
