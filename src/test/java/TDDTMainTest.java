0154ab5fae3cb69cab0f1fc89606dda8c8cf7f5b
Anne Abraham <anne.abraham@hhu.de>
2016-06-22 18:28:04 +0200

    Beispiel Test f. JavaFX

--- src/test/java/TDDTMainTest.java
+++ src/test/java/TDDTMainTest.java
@@ -4,5 +4,30 @@ import static org.junit.Assert.*;
 
 public class TDDTMainTest
 {
-//   Hier könnten deine Tests stehen
+/*
+@Test
+    public void testA() throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                new JFXPanel(); // Initializes the JavaFx Platform
+                Platform.runLater(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        new Main().start(new Stage()); // Create and
+                                                        // initialize
+
+
+                    }
+                });
+            }
+        });
+        thread.start();// Initialize the thread
+        Thread.sleep(10000); // Time to use the app, with out this, the thread
+                                // will be killed before you can tell.
+    }
+
+ */
 }
