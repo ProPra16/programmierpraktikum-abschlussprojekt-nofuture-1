4d6f5927d4b4a66346374ccd4b9a46e23eed9386
timat102 <tim.matussek@hhu.de>
2016-07-01 14:24:55 +0200

    implemented InterfaceTDDT in class Phases

---
+++ src/main/java/phases/Phases.java
@@ -0,0 +1,15 @@
+package phases;
+
+public class Phases implements InterfaceTDDT{
+
+    private String phase;
+
+    public String getPhase() {
+        return phase;
+    }
+
+    public void setPhase(String phase) {
+        this.phase = phase;
+    }
+
+}
