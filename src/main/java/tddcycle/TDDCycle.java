35aaae2851a796aaf324c965777e44f3a2382d23
timat102 <tim.matussek@hhu.de>
2016-07-04 13:44:14 +0200

    added method compile

--- src/main/java/tddcycle/TDDCycle.java
+++ src/main/java/tddcycle/TDDCycle.java
@@ -1,11 +1,27 @@
 package tddcycle;
 
 import phases.Phases;
+import vk.core.api.*;
+
+import java.util.Collection;
 
 public class TDDCycle extends Phases{
 
     public TDDCycle(String phase) {
         super(phase);
     }
-    
+
+    private TestResult compile(String code, String test){
+        CompilationUnit codeUnit = new CompilationUnit("Code", code, false);
+        CompilationUnit testUnit = new CompilationUnit("Test", test, true);
+        JavaStringCompiler compiler = CompilerFactory.getCompiler(codeUnit, testUnit);
+        compiler.compileAndRunTests();
+        CompilerResult compilerResult = compiler.getCompilerResult();
+        if(compilerResult.hasCompileErrors()) {
+            Collection<CompileError> compileErrors = compilerResult.getCompilerErrorsForCompilationUnit(testUnit);
+            compileErrors.addAll(compilerResult.getCompilerErrorsForCompilationUnit(codeUnit));
+            compileErrors.forEach(System.out::println);
+        }
+        return compiler.getTestResult();
+    }
 }
