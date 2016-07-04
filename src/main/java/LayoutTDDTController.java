a7fe025e6fbbb122286c4949daa10f97bb9c8149
Cedric Wolffs <cedric.wolffs@hhu.de>
2016-07-04 19:23:13 +0200

    Mords Hackerskills
    Phillip und Cedric rasten aus

--- src/main/java/LayoutTDDTController.java
+++ src/main/java/LayoutTDDTController.java
@@ -1,13 +1,27 @@
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
 
 public class LayoutTDDTController {
 
    @FXML
-   static Label exerciseTDDT;
+   Label exerciseTDDT;
 
    public static void setzeAufgabe(String exerciseText)
    {
-      exerciseTDDT.setText(exerciseText);
+      FXMLLoader fxmlLoader = new FXMLLoader(LayoutTDDTController.class.getResource("layoutTDDT.fxml"));
+      try
+      {
+         fxmlLoader.load();
+         fxmlLoader.getController();
+         LayoutTDDTController tddtController = fxmlLoader.getController();
+         tddtController.exerciseTDDT.setText(exerciseText);
+      } catch (IOException e)
+      {
+         e.printStackTrace();
+      }
+
    }
 }
