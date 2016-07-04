68bdf0cedac6612e1d0fd0acb602fa3c9c988f5b
timat102 <tim.matussek@hhu.de>
2016-07-04 13:24:01 +0200

    more Tests

--- src/test/java/phases/PhasesTest.java
+++ src/test/java/phases/PhasesTest.java
@@ -7,8 +7,13 @@ import static org.junit.Assert.assertEquals;
 public class PhasesTest {
     @Test
     public void Test1(){
-        Phases p = new Phases();
-        p.setPhase("green");
+        Phases p = new Phases("green");
         assertEquals("green", p.getPhase());
+    }
+
+    @Test
+    public void Test2(){
+        Phases p = new Phases("abc");
+        assertEquals("abc", p.getPhase());
     }
 }
