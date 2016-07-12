d2326197872419ed927d1313c2fce74c9a0fee7e
timat102 <tim.matussek@hhu.de>
2016-07-12 17:06:07 +0200

    Babysteps now contains timeline

--- src/main/java/babysteps/Babysteps.java
+++ src/main/java/babysteps/Babysteps.java
@@ -1,76 +1,43 @@
 package babysteps;
 
-public class Babysteps {
-    int timer;
-    String  code, testCode;
-    String phase;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.util.Duration;
 
-    public Babysteps(String phase, String code, String testCode, int timer) {
-        this.phase = phase;
-        this.code = code;
-        this.testCode = testCode;
-        this.timer = timer;
-        babysteps();
+import java.util.function.Function;
+
+public class Babysteps extends SimpleStringProperty{
+
+    private Function callback;
+    private int max_time, time;
+    private Timeline timeline;
+
+    public Babysteps(int max_time, Function callback) {
+        reset();
+        this.max_time = max_time;
+        this.callback = callback;
+        timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1), (ActionEvent e) -> {
+                time++;
+                if(time >= max_time) {
+                    callback.apply(null);
+                    reset();
+                }
+        }));
     }
 
-    public void babysteps(){
-
-
-        if (timer==0) {
-            switch (phase) {
-
-                case "red":
-                    setPhase("refactor");
-
-                    setCode("public class Class {\n  // TODO\n}");
-                    setTestCode("import static org.junit.Assert.*;\nimport org.junit.Test;\npublic class TestClass {\n  @Test\n  public void test() {\n    // TODO\n  }\n}");
-
-
-                    //Code löschen
-                    //GOTO Phase REFACTOR
-
-                    break;
-                case "green":
-
-                    setPhase("red");
-
-                    setCode("public class Class {\n  // TODO\n}");
-                    setTestCode("import static org.junit.Assert.*;\nimport org.junit.Test;\npublic class TestClass {\n  @Test\n  public void test() {\n    // TODO\n  }\n}");
-
-                    //Code löschen
-                    //GOTO RED
-
-
-                    break;
-
-                default: break;
-            }
-
-        }
-    }
-    public void setPhase(String s){
-
-        phase = s;
+    public void reset(){
+        this.time = 0;
     }
 
-    public String getPhase(){
-
-        return phase;
+    public void start(){
+        timeline.play();
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public void setTestCode(String testCode) {
-        this.testCode = testCode;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public String getTestCode() {
-        return testCode;
+    public void stop(){
+        timeline.stop();
     }
 }
